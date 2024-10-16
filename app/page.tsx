f8b41386d51c5a406071d764a7b86015fbfc2524
Suboch. S <sema.chel12@gmail.com>
2024-10-16 13:45:36 +0300

    top-bar:categories+sortPopup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import { Container, Title } from '@/components/shared';
-import { Button } from '@/components/ui/button';
+import { Container, Filters, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
 	return (
 		<>
 			<Container className="mt-10">
 				<Title text="Все игры" size="lg" className="font-extrabold" />
+			</Container>
+			<TopBar />
+			<Container className="pb-14">
+				<div className="flex gap-[60px]">
+					{/*Филтьтрация*/}
+					<div className="w-[250px]">
+						<Filters />
+					</div>
+				</div>
 			</Container>
 		</>
 	);
