a1b4991c0aafd84d2ce2fc9fa4cbe8a7f7ea7639
Suboch. S <sema.chel12@gmail.com>
2024-10-16 09:56:36 +0300

    подключение шрифта гугл

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
 	return (
 		<h1>
-			hello
-			<Button>Korzina</Button>
+			Home
+			<Button variant="outline">Корзина</Button>
 		</h1>
 	);
 }
