f81cf51b3e841f3689972d1566903ee974608100
Suboch. S <sema.chel12@gmail.com>
2024-10-17 20:56:14 +0300

    filter-checkbox

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Title } from '.';
+import { FilterCheckbox } from './filter-checkbox';
 
 interface Props {
 	className?: string;
@@ -9,8 +10,9 @@ export const Filters: React.FC<Props> = ({ className }) => {
 	return (
 		<div className={className}>
 			<Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
-			<div className='flex flex-col gap-4'>
-				<ch
+			<div className="flex flex-col gap-4">
+				<FilterCheckbox text="можно собрать" value="1" />
+				<FilterCheckbox text="Новинки" value="2" />
 			</div>
 		</div>
 	);
