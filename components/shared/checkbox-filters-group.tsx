85a6c04f89944b6e3b3035437068fb32697bd913
Semyon <sema.chel12@gmail.com>
2024-10-18 20:18:48 +0300

    range с 2умя ползунками Range-slider

---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface Props {
+	className?: string;
+}
+export const CheckboxFiltersGroup: React.FC<Props> = ({ className }) => {
+	return <div className={className}>
+		
+	</div>;
+};
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Title } from '.';
 import { FilterCheckbox } from './filter-checkbox';
-import { Input } from '../ui';
+import { Input, RangeSlider } from '../ui';
 
 interface Props {
 	className?: string;
@@ -11,10 +11,12 @@ export const Filters: React.FC<Props> = ({ className }) => {
 	return (
 		<div className={className}>
 			<Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
+			{/*Верхние чекбоксы*/}
 			<div className="flex flex-col gap-4">
 				<FilterCheckbox text="можно собрать" value="1" />
 				<FilterCheckbox text="Новинки" value="2" />
 			</div>
+			{/*Фильтр цен*/}
 			<div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
 				<p className="font-bold mb-3">Цена от и до:</p>
 				<div className="flex gap-3 mb-5">
@@ -32,7 +34,9 @@ export const Filters: React.FC<Props> = ({ className }) => {
 						placeholder="10000"
 					/>
 				</div>
+				<RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
 			</div>
+			{/* Содержимое*/}
 		</div>
 	);
 };
