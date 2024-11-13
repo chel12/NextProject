3a32a35fe97900e01a9355dadf6c2b5438e8d664
Semyon <sema.chel12@gmail.com>
2024-11-13 17:20:20 +0300

    add api:product;users. (get,post)

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,17 @@
 import { prisma } from '@/prisma/prisma-client';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
 	const users = await prisma.user.findMany();
 	return NextResponse.json(users);
+}
+
+export async function POST(req: NextRequest) {
+	//получить data из запроса
+	const data = await req.json();
+	//ждём создание пользователя в БД
+	const user = await prisma.user.create({
+		data,
+	});
+	return NextResponse.json(user);
 }
