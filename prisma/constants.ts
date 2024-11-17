47d0d24eae4772d28d80c4e6e5be4c554cf156c6
Semyon <sema.chel12@gmail.com>
2024-11-17 20:55:24 +0300

    update seed.ts

---
+++ prisma/constants.ts
@@ -0,0 +1,89 @@
+export const categories = [
+	{
+		name: 'Шутеры',
+	},
+	{
+		name: 'Экшен',
+	},
+	{
+		name: 'Стратегия',
+	},
+	{
+		name: 'Гонки',
+	},
+	{
+		name: 'Симуляторы',
+	},
+	{
+		name: 'Военная тематика',
+	},
+	{
+		name: 'Выживание',
+	},
+	{
+		name: 'Хоррор',
+	},
+];
+export const ingredients = [
+	{
+		name: 'классическая упаковка',
+		price: 10,
+		ImageUrl: 'https://cdlabs.ru/wp-content/uploads/2016/06/DVD-Box.png',
+	},
+	{
+		name: 'праздничная упаковка',
+		price: 15,
+		ImageUrl:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmNXodlCKSqSLPMswykd4skmK80VZ3Ina4RQ&s',
+	},
+	{
+		name: 'прозрачная упаковка',
+		price: 15,
+		ImageUrl:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX8TGO1f94IvBNcBK_-K7YiVEqBc_4J3xwVw&s',
+	},
+	{
+		name: 'кастомная упаковка',
+		price: 100,
+		ImageUrl:
+			'https://img.freepik.com/free-psd/christmas-gift-isolated_23-2151250408.jpg',
+	},
+].map((obj, index) => ({ id: index + 1, ...obj }));
+export const products = [
+	{
+		name: 'Product 1',
+		categoryId: 1,
+		imageUrl:
+			'https://e7.pngegg.com/pngimages/1008/303/png-clipart-shopping-cart-icon-product-return-shopping-cart-retail-supermarket-thumbnail.png',
+	},
+	{
+		name: 'Product 2',
+		categoryId: 1,
+		imageUrl:
+			'https://e7.pngegg.com/pngimages/1008/303/png-clipart-shopping-cart-icon-product-return-shopping-cart-retail-supermarket-thumbnail.png',
+	},
+	{
+		name: 'Product 3',
+		categoryId: 2,
+		imageUrl:
+			'https://e7.pngegg.com/pngimages/1008/303/png-clipart-shopping-cart-icon-product-return-shopping-cart-retail-supermarket-thumbnail.png',
+	},
+	{
+		name: 'Product 4',
+		categoryId: 2,
+		imageUrl:
+			'https://e7.pngegg.com/pngimages/1008/303/png-clipart-shopping-cart-icon-product-return-shopping-cart-retail-supermarket-thumbnail.png',
+	},
+	{
+		name: 'Product 5',
+		categoryId: 3,
+		imageUrl:
+			'https://e7.pngegg.com/pngimages/1008/303/png-clipart-shopping-cart-icon-product-return-shopping-cart-retail-supermarket-thumbnail.png',
+	},
+	{
+		name: 'Product 6',
+		categoryId: 3,
+		imageUrl:
+			'https://e7.pngegg.com/pngimages/1008/303/png-clipart-shopping-cart-icon-product-return-shopping-cart-retail-supermarket-thumbnail.png',
+	},
+];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,27 @@
 import { hashSync } from 'bcrypt';
 import { prisma } from './prisma-client';
+import { categories, ingredients, products } from './constants';
+import { Prisma } from '@prisma/client';
+
+const randomNumber = (min: number, max: number) => {
+	return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+const generateProductItem = ({
+	productId,
+	gameType,
+	size,
+}: {
+	productId: number;
+	gameType?: 1 | 2;
+	size?: 20 | 30 | 40;
+}) => {
+	return {
+		productId,
+		price: randomNumber(100, 1000),
+		gameType,
+		size,
+	} as Prisma.ProductItemUncheckedCreateInput;
+};
 
 async function up() {
 	await prisma.user.createMany({
@@ -20,10 +42,109 @@ async function up() {
 			},
 		],
 	});
+	await prisma.category.createMany({
+		data: categories,
+	});
+	await prisma.ingredient.createMany({
+		data: ingredients,
+	});
+	await prisma.product.createMany({
+		data: products,
+	});
+	const game1 = await prisma.product.create({
+		data: {
+			name: 'Call of Duty 4',
+			imageUrl:
+				'https://gaming-cdn.com/images/products/1620/orig/call-of-duty-4-modern-warfare-pc-mac-game-steam-cover.jpg?v=1701179820',
+			categoryId: 1,
+			ingredients: {
+				connect: ingredients.slice(0, 4),
+			},
+		},
+	});
+	const game2 = await prisma.product.create({
+		data: {
+			name: 'Mafia 2',
+			imageUrl:
+				'https://cdn.kanobu.ru/games/72/25478a08b5414e78a1cb4764e959004d',
+			categoryId: 2,
+			ingredients: {
+				connect: ingredients.slice(0, 4),
+			},
+		},
+	});
+	const game3 = await prisma.product.create({
+		data: {
+			name: 'Far Cry 3',
+			imageUrl:
+				'https://cdn.kanobu.ru/games/31/f19e86587b7f4341a77839717de01841',
+			categoryId: 2,
+			ingredients: {
+				connect: ingredients.slice(0, 4),
+			},
+		},
+	});
+	await prisma.productItem.createMany({
+		data: [
+			//game 1
+			generateProductItem({ productId: game1.id, gameType: 1, size: 20 }),
+			generateProductItem({ productId: game1.id, gameType: 2, size: 40 }),
+			//game 2
+			generateProductItem({ productId: game2.id, gameType: 1, size: 20 }),
+			generateProductItem({ productId: game2.id, gameType: 1, size: 30 }),
+			generateProductItem({ productId: game2.id, gameType: 1, size: 40 }),
+			generateProductItem({ productId: game2.id, gameType: 2, size: 20 }),
+			generateProductItem({ productId: game2.id, gameType: 2, size: 30 }),
+			generateProductItem({ productId: game2.id, gameType: 2, size: 40 }),
+			//game 3
+			generateProductItem({ productId: game3.id, gameType: 1, size: 20 }),
+			generateProductItem({ productId: game3.id, gameType: 2, size: 20 }),
+			generateProductItem({ productId: game3.id, gameType: 2, size: 30 }),
+			//остальные игры
+			generateProductItem({ productId: 1 }),
+			generateProductItem({ productId: 2 }),
+			generateProductItem({ productId: 3 }),
+			generateProductItem({ productId: 4 }),
+			generateProductItem({ productId: 5 }),
+			generateProductItem({ productId: 6 }),
+			generateProductItem({ productId: 7 }),
+			generateProductItem({ productId: 8 }),
+		],
+	});
+	await prisma.cart.createMany({
+		data: [
+			{
+				userId: 1,
+				totalAmount: 0,
+				token: '11111',
+			},
+			{
+				userId: 2,
+				totalAmount: 0,
+				token: '22222',
+			},
+		],
+	});
+	await prisma.cartItem.create({
+		data: {
+			productItemId: 1,
+			cartId: 1,
+			quantity: 2,
+			ingredients: {
+				connect: [{ id: 1 }, { id: 2 }, { id: 3 }],
+			},
+		},
+	});
 }
 async function down() {
 	//чтобы инкремент тоже очистить, прямой запрос
-	await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+	await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+	await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
 }
 async function main() {
 	try {
