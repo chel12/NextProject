943034e35da47ea0b720d9ad47251017cc953d54
Semyon <sema.chel12@gmail.com>
2024-11-24 15:15:36 +0300

    api:products-get(query) с урла берём и в бд обращаемся

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -1,6 +1,20 @@
+import { prisma } from '@/prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-	console.log(req.nextUrl);
-	return NextResponse.json({});
+	// console.log(req.nextUrl.searchParams.get('query'));
+	//берем с урла
+	const query = req.nextUrl.searchParams.get('query')||''
+	//В БД обращаемся
+	const products = await prisma.product.findMany({
+		where:{
+			name:{
+					contains:query,
+					mode:'insensitive'
+				}
+		},
+		//кол-во которое хотим вернуть
+		take:5
+	})
+	return NextResponse.json(products);
 }
