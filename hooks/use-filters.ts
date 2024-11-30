c2ae638f28bade3977828228142d7b841d7b22ab
Semyon <sema.chel12@gmail.com>
2024-11-30 20:15:54 +0300

    refactoring v1 filters & hooks

---
+++ hooks/use-filters.ts
@@ -0,0 +1,76 @@
+import { useSearchParams } from 'next/navigation';
+import { useSet } from 'react-use';
+import React from 'react';
+
+interface PriceProps {
+	priceFrom?: number;
+	priceTo?: number;
+}
+interface QueryFilters extends PriceProps {
+	gameTypes: string;
+	platforms: string;
+	ingredients: string;
+}
+export interface Filters {
+	platforms: Set<string>;
+	gameTypes: Set<string>;
+	selectedIngredients: Set<string>;
+	prices: PriceProps;
+}
+interface ReturnProps extends Filters {
+	setPrices: (name: keyof PriceProps, value: number) => void;
+	setGameType: (key: string) => void;
+	setPlatform: (key: string) => void;
+	setIngredients: (key: string) => void;
+}
+
+export const useFilters = (): ReturnProps => {
+	//url и queryString
+
+	const searchParams = useSearchParams() as unknown as Map<
+		keyof QueryFilters,
+		string
+	>;
+	/*Фильтр ингредиентов*/
+	const [selectedIngredients, { toggle: toggleIngredients }] = useSet(
+		new Set<string>(searchParams.get('ingredients')?.split(','))
+	);
+	/*Фильтр платформ*/
+	const [platforms, { toggle: togglePlatform }] = useSet(
+		new Set<string>(
+			searchParams.has('platforms')
+				? searchParams.get('platforms')?.split(',')
+				: []
+		)
+	);
+	/*Фильтр типов изданий*/
+	const [gameTypes, { toggle: toggleGameType }] = useSet(
+		new Set<string>(
+			searchParams.has('gameTypes')
+				? searchParams.get('gameTypes')?.split(',')
+				: []
+		)
+	);
+	/*Фильтр стоимости*/
+	const [prices, setPrices] = React.useState<PriceProps>({
+		priceFrom: Number(searchParams.get('priceFrom')) || undefined,
+		priceTo: Number(searchParams.get('priceTo')) || undefined,
+	});
+	const updatePrice = (name: keyof PriceProps, value: number) => {
+		setPrices({
+			...prices,
+			[name]: value,
+		});
+	};
+
+	return {
+		platforms,
+		gameTypes,
+		prices,
+		selectedIngredients,
+		setPrices: updatePrice,
+		setGameType: toggleGameType,
+		setPlatform: togglePlatform,
+		setIngredients: toggleIngredients,
+	};
+};
--- hooks/use-ingredients.ts
+++ hooks/use-ingredients.ts
@@ -1,20 +1,10 @@
-import { Ingredient } from '@prisma/client';
 import { Api } from '@/services/api-client';
+import { Ingredient } from '@prisma/client';
 import React from 'react';
-import { useSet } from 'react-use';
 
-interface ReturnProps {
-	ingredients: Ingredient[];
-	loading: boolean;
-	selectedIngredients: Set<string>;
-	onAddId: (id: string) => void;
-}
-
-export const useFilterIngredients = (values: string[] = []): ReturnProps => {
-	const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
+export const useIngredients = () => {
 	const [loading, setLoading] = React.useState<boolean>(true);
-	const [selectedIngredients, { toggle }] = useSet(new Set<string>(values));
-
+	const [ingredients, setIngredients] = React.useState<Ingredient[]>([]);
 	React.useEffect(() => {
 		async function fetchIngredients() {
 			try {
@@ -38,11 +28,5 @@ export const useFilterIngredients = (values: string[] = []): ReturnProps => {
 
 		fetchIngredients();
 	}, []);
-
-	return {
-		ingredients,
-		loading,
-		onAddId: toggle,
-		selectedIngredients,
-	};
+	return { ingredients, loading };
 };
