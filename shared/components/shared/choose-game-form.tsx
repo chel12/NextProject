eb877239ef4860543d39eba8f1472699ad1206e1
Suboch. S <sema.chel12@gmail.com>
2024-12-16 09:56:08 +0300

    доп div для скролла внутри ингредиентов

--- shared/components/shared/choose-game-form.tsx
+++ shared/components/shared/choose-game-form.tsx
@@ -39,7 +39,6 @@ export const ChooseGameForm: React.FC<Props> = ({
 			<div className="w-[490px] bg-[#F7F6F5] p-7">
 				<Title text={name} size="md" className="font-extrabold mb-1" />
 				<p className="text-gray-400">{textDetails}</p>
-
 				<div className="flex flex-col gap-4 mt-5">
 					<GroupVariants
 						items={gameEdition}
@@ -54,16 +53,20 @@ export const ChooseGameForm: React.FC<Props> = ({
 						onClick={(value) => setType(Number(value) as GameType)}
 					/>
 				</div>
-				<div className="grid grid-cols-3 gap-3">
-					{ingredients.map((ingredient) => (
-						<IngredientItem
-							key={ingredient.id}
-							name={ingredient.name}
-							price={ingredient.price}
-							imageUrl={ingredient.ImageUrl}
-							onClick={onClickAdd}
-						/>
-					))}
+				{/*div для скролла в ингредиентах(scrollbar кастом класс из
+				global css)*/}
+				<div className="bg-gray-50 p-5 rounded-md h-[420px] overflow-auto scrollbar mt-5">
+					<div className="grid grid-cols-3 gap-3">
+						{ingredients.map((ingredient) => (
+							<IngredientItem
+								key={ingredient.id}
+								name={ingredient.name}
+								price={ingredient.price}
+								imageUrl={ingredient.ImageUrl}
+								onClick={onClickAdd}
+							/>
+						))}
+					</div>
 				</div>
 				<Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
 					Добавить в корзину за {totalPrice} Р
