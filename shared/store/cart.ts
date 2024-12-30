edf399ccf1aa145989fe0ce167a18630ca566a52
Semyon <sema.chel12@gmail.com>
2024-12-30 12:03:11 +0300

    store:removeCartItem

--- shared/store/cart.ts
+++ shared/store/cart.ts
@@ -47,6 +47,17 @@ export const useCartStore = create<CartState>((set, get) => ({
 			set({ loading: false });
 		}
 	},
-	removeCartItem: async (id: number) => {},
+	removeCartItem: async (id: number) => {
+		try {
+			set({ loading: true, error: false });
+			const data = await Api.cart.removeCartItem(id);
+			set(getCartDetails(data));
+		} catch (error) {
+			console.error(error);
+			set({ error: true });
+		} finally {
+			set({ loading: false });
+		}
+	},
 	addCartItem: async (values: any) => {},
 }));
